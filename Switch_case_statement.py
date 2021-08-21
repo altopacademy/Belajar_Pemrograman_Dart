eb2dd507902e35e2785f09c80b8d4f6e93d925b7
helloaltop <helloaltop@gmail.com>
2021-08-21 07:59:36 +0700

    update

---
+++ Switch_case_statement.py
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+    int number = 1;
+
+    switch(number){
+        case 0:
+            print("nol");
+            break;
+        case 1:
+            print("satu");
+            break;
+    }
+}
